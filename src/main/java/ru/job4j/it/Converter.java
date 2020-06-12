22c5236b3e5ba909a9320707cbab39fb9d2a41ad
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-06-12 17:33:36 +0300

    5.1.4. Создать convert(Iterator<Iterator>) [#299908]

---
+++ src/main/java/ru/job4j/it/Converter.java
@@ -0,0 +1,29 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Converter {
+    public Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+
+            private Iterator<Integer> curIt = it.next();
+
+            @Override
+            public boolean hasNext() {
+                while (!curIt.hasNext() && it.hasNext()) {
+                    curIt = it.next();
+                }
+                return curIt.hasNext();
+            }
+
+            @Override
+            public Integer next() {
+                if (!hasNext() && !it.hasNext() && curIt.hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return curIt.next();
+            }
+        };
+    }
+}
--- src/main/java/ru/job4j/it/Test.java
+++ src/main/java/ru/job4j/it/Test.java
@@ -1,22 +1,23 @@
 package ru.job4j.it;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class Test {
     public static void main(String[] args) {
-/*        int[] in = new int[0];
-        int[] in2 = {};*/
-        /*int[][] in = {
-                {}
-        };
-        for (int[] el : in) {
-            for (int el1 : el) {
-                System.out.println(el1);
-            }
-        }*/
-        int[] in2 = {1};
-        System.out.println(in2.length);
+        Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
+        Iterator<Integer> it2 = Arrays.asList(4, 5, 6).iterator();
+        Iterator<Integer> it3 = Arrays.asList(7, 8, 9).iterator();
+        Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
+        Iterator<Integer> it4 = Collections.emptyIterator();
 
+        System.out.println(it1.next());
+        System.out.println(it1.next());
+        System.out.println(it1.next());
+        System.out.println(its.hasNext());
+        System.out.println(its.next());
+        System.out.println(it4.hasNext());
     }
 
 }
