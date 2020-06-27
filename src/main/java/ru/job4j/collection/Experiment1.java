cf241a4655a3c4f4def0d327f6914315b5380356
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-06-27 19:06:20 +0300

    1. Создать модель User [#299928] исправления

---
+++ src/main/java/ru/job4j/collection/Experiment1.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Experiment1 {
+    User obj1 = new User("Mark0", 0, new GregorianCalendar(1900, 0,1));
+    User obj2 = new User("Mark0", 0, new GregorianCalendar(1900, 0,1));
+    Map<User, Object> map1 = new HashMap<>();
+
+    @Test
+    public void map() {
+        map1.put(obj1, "first");
+        map1.put(obj2, "second");
+        System.out.println(map1 );
+
+    }
+
+
+}
