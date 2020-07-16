33e5ed7fa799cd8344f2f1c156ad09622897fd0a
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-07-16 16:35:44 +0300

    0.1. FileOutputStream. [#299948]

---
+++ src/main/java/ru/job4j/io/TabPrint.java
@@ -0,0 +1,37 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class TabPrint {
+    public static int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                table[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return table;
+    }
+
+    public static void main(String[] args) {
+        int size = 3;
+        int[][] table = TabPrint.multiple(size);
+        try (FileOutputStream out = new FileOutputStream("Table.txt")) {
+            for (var row : table) {
+                size = 3;
+                for (var el : row) {
+                    String forBuff = Integer.toString(el);
+                    forBuff = forBuff + " ";
+                    if (size == 1) {
+                        forBuff = forBuff + System.lineSeparator();
+                    }
+                    byte[] buffer = forBuff.getBytes();
+                    out.write(buffer);
+                    size = size - 1;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
