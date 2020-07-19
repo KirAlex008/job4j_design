6499395e20c7fa094a9f480e91c530e9f05e59a3
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-07-19 18:43:56 +0300

    0.2. FileInputStream [#299957] исправления

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -10,18 +10,9 @@ public class EvenNumberFile {
             while ((read = in.read()) != -1) {
                 text.append((char) read);
             }
-            char[] buffer = new char[text.length()];
-            String str = "";
-            text.getChars(0, text.length(), buffer, 0);
-            for (var el : buffer) {
-                if (el == 10 || el == 13) {
-                    str = str + " ";
-                } else {
-                    str = str + el;
-                }
-            }
-            for (String el : str.split("  ")) {
-                Integer.parseInt(el);
+            //String a = "\\u000A";
+            String[] array = text.toString().split("\r?\n");
+            for (String el : array) {
                 if (Integer.parseInt(el) % 2 != 0) {
                     System.out.println(el);
                 }
