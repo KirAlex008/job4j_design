6cced20d5d30f221d64fed9a56ea1d69b372b3d5
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-07-31 10:31:57 +0300

    5.2. Архивировать проект [#299955] исправления

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -33,18 +34,22 @@ public class Zip {
         }
     }
 
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> !p.toFile().getName().endsWith(ext)) ;
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
         String ext = "";
-    ArgZip argZip = new ArgZip(args);
-    if (argZip.valid()) {
-        start = Paths.get(args[0].substring(3));
-        ext = args[1].substring(3);
-    }
-        var sources = Search.search(start, ext);
-        System.out.println(sources);
-    Zip zipper = new Zip();
+        ArgZip argZip = new ArgZip(args);
+        if (argZip.valid()) {
+            start = Paths.get(args[0].substring(3));
+            ext = args[1].substring(3);
+        }
+        var sources = search(start, ext);
+        Zip zipper = new Zip();
         zipper.packFiles(sources, new File(args[2].substring(3)));
-
     }
 }
