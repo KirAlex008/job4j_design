fef9f53a520834f3a6120b74daf4e8089995e7d3
KirAlex008 <kirshaav@yandex.ru>
2020-09-18 16:25:00 +0300

    Хранилище машин [#299886]

---
+++ src/main/java/ru/job4j/database/car_catalog.sql
@@ -0,0 +1,52 @@
+create table body (
+                      id serial primary key,
+                      name varchar(200)
+);
+
+create table engine (
+                        id serial primary key,
+                        name varchar(200)
+);
+
+create table transmission (
+                              id serial primary key,
+                              name varchar(200)
+);
+
+create table car (
+                     id serial primary key,
+                     name varchar(200),
+                     body_id integer references body,
+                     engine_id integer references engine,
+                     transmission_id integer references transmission
+);
+
+insert into body(name) values ('body_1');
+insert into body(name) values ('body_2');
+insert into body(name) values ('body_3');
+insert into body(name) values ('body_4');
+
+insert into engine(name) values ('engine_1');
+insert into engine(name) values ('engine_2');
+insert into engine(name) values ('engine_3');
+insert into engine(name) values ('engine_4');
+
+insert into transmission(name) values ('transmission_1');
+insert into transmission(name) values ('transmission_2');
+insert into transmission(name) values ('transmission_3');
+insert into transmission(name) values ('transmission_4');
+
+insert into car(name, body_id, engine_id, transmission_id) values ('car_1', 2, 3, 4);
+insert into car(name, body_id, engine_id, transmission_id) values ('car_2', 3, 4, 1);
+insert into car(name, body_id, engine_id, transmission_id) values ('car_3', 4, 1, 2);
+
+
+select c.name, c.body_id, c.engine_id, c.transmission_id from car as c;
+
+select  b.name from body as b left outer join car as c on b.id = c.body_id where c.body_id is null;
+
+select  e.name from engine as e left outer join car as c on e.id = c.engine_id where c.engine_id is null;
+
+select  t.name from transmission as t left outer join car as c on t.id = c.transmission_id where c.transmission_id is null;
+
+
